baab1bd11cca1270588965305212e91e27699851
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-06-17 19:15:33 -0300

    ip

--- poupai-app/src/services/apiBase.ts
+++ poupai-app/src/services/apiBase.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const url: string = 'http://192.168.0.4:8080';
+export const url: string = 'http://10.10.102.58:8080';
 
 export const getToken = async () => {
   return await AsyncStorage.getItem("token");
